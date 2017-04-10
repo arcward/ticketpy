4ae63af96f10dbf5507c632743744dbf62bb7733
Edward Wells <git@edward.sh>
2017-04-09 20:53:54 -0400

    Converted README to rst, updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
-    name = 'ticketpy',
-    version = '1.0.2',
-    author = 'Edward Wells',
-    author_email = 'git@edward.sh',
-    description = "Python client/library for the Ticketmaster discovery API",
-    license = 'MIT',
-    keywords = 'Ticketmaster',
-    url = 'https://github.com/arcward/ticketpy',
-    packages = ['ticketpy'], install_requires=['requests']
+    name='ticketpy',
+    version='1.0.2',
+    author='Edward Wells',
+    author_email='git@edward.sh',
+    description="Python client/library for the Ticketmaster discovery API",
+    long_description=read('README.rst'),
+    license='MIT',
+    keywords='Ticketmaster',
+    url='https://github.com/arcward/ticketpy',
+    packages=['ticketpy'], install_requires=['requests']
 )
