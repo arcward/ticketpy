f11e461edb2787ce998b839786957439b0cfe920
Edward Wells <git@edward.sh>
2017-04-13 02:37:20 -0400

    rearranged classes

--- ticketpy/ticketpy.py
+++ ticketpy/ticketpy.py
@@ -140,6 +140,145 @@ class ApiException(Exception):
         return '\n'.join(msgs)
 
 
+class PageIterator:
+    """Iterates through API response pages"""
+
+    def __init__(self, api_client, **kwargs):
+        self.api_client = api_client  #: Parent API client
+        self.page = None  #: Current page
+        self.page = self.__page(**kwargs)
+
+        self.start_page = self.page.number  #: Initial page number
+        self.current_page = self.start_page  #: Current page number
+        self.end_page = self.page.total_pages  #: Final page number
+
+    def __iter__(self):
+        return self
+
+    def limit(self, max_pages=10):
+        """Limit the number of page requests. Default: 5
+
+        With a default page size of 20, ``limit(max_pages=5`` would 
+        return a maximum of 200 items (fewer, if there are fewer results).
+
+        Use this to contain the number of API calls being made, as the 
+        API restricts users to a maximum of 5,000 per day. Very 
+        broad searches can return a large number of pages.
+
+        To contrast, ``all()`` will automatically request every 
+        page available.
+
+        :param max_pages: Maximum number of pages to request. 
+            Default: *10*. Set to *None* (or use ``all()``) to return 
+            all pages.
+        :return: Flat list of results from pages
+        """
+        if max_pages is None:
+            return self.all()
+
+        all_items = []
+        for i in range(0, max_pages):
+            all_items += self.next()
+        return all_items
+
+    def all(self):
+        """Returns a flat list of all results. Queries all possible pages.
+
+        Use ``limit()`` to restrict the number of calls being made.
+
+        :return: Flat list of results
+        """
+        return [i for item_list in self for i in item_list]
+
+    @staticmethod
+    def __page(**kwargs):
+        """Instantiate and return a Page(list)"""
+        page = kwargs['page']
+
+        links = kwargs['_links']
+
+        if 'next' not in links:
+            links_next = None
+        else:
+            links_next = links['next']['href']
+
+        return Page(
+            page['number'],
+            page['size'],
+            page['totalElements'],
+            page['totalPages'],
+            links['self']['href'],
+            links_next,
+            kwargs.get('_embedded', {})
+        )
+
+    def next(self):
+        # Return initial Page result if we haven't yet
+        if self.page.number == self.current_page:
+            self.current_page += 1
+            return [i for i in self.page]
+
+        # StopIteration if we know we've run out of pages.
+        # Check for current>end as empty results still return
+        # a page and increment the counter.
+        if self.current_page >= self.end_page:
+            raise StopIteration
+
+        # Otherwise, +1 our count and pull the next page
+        self.current_page += 1
+        r = requests.get(self.page.link_next,
+                         params=self.api_client.api_key).json()
+
+        self.page = self.__page(**r)
+
+        # If 'next' link goes missing, there were fewer pages than
+        # expected for some reason, so bump current_page to end_page to
+        # throw StopIteration next time next() is called
+        if self.page.link_next is None:
+            self.current_page = self.end_page
+
+        return [i for i in self.page]
+
+    __next__ = next
+
+
+class Page(list):
+    """API response page"""
+
+    def __init__(self, number, size, total_elements, total_pages,
+                 link_self, link_next, embedded):
+        super().__init__([])
+        self.number = number  #: Page number
+        self.size = size  #: Page size
+        self.total_elements = total_elements  #: Total elements (all pages)
+        self.total_pages = total_pages  #: Total pages
+
+        self._link_self = link_self  #: Link to this page
+        self._link_next = link_next  #: Link to the next page
+
+        # Parse embedded objects and add them to ourself
+        items = []
+        if 'events' in embedded:
+            items = [Event.from_json(e) for e in embedded['events']]
+        elif 'venues' in embedded:
+            items = [Venue.from_json(v) for v in embedded['venues']]
+        elif 'attractions' in embedded:
+            items = [Attraction.from_json(a) for a in embedded['attractions']]
+
+        for i in items:
+            self.append(i)
+
+    @property
+    def link_next(self):
+        """Link to the next page"""
+        return "{}{}".format(ApiClient.base_url, self._link_next)
+
+    @property
+    def link_self(self):
+        """Link to this page"""
+        return "{}{}".format(ApiClient.base_url, self._link_self)
+
+
 class _VenueSearch:
     """Queries for venues"""
 
@@ -453,39 +592,6 @@ class _AttractionSearch:
         return self.__get(**search_params)
 
 
-class Attraction:
-    """Attraction object"""
-    def __init__(self, attraction_id=None, attraction_name=None, url=None,
-                 classifications=None, images=None, test=None):
-        """
-        
-        :param attraction_id: Attraction ID
-        :param attraction_name: Name of the attraction (ex: '*U2*'
-        :param url: Ticketmaster URL
-        :param classifications: Genres, subgenres, etc
-        :param images: Images
-        :param test: Test (bool)
-        """
-        self.id = attraction_id
-        self.name = attraction_name
-        self.url = url
-        self.classifications = classifications
-        self.images = images
-        self.test = test
-
-    @staticmethod
-    def from_json(json_obj):
-        """Convert JSON object to ``Attraction`` object"""
-        att = Attraction()
-        att.id = json_obj.get('id')
-        att.name = json_obj.get('name')
-        att.url = json_obj.get('url')
-        att.test = json_obj.get('test')
-        att.images = json_obj.get('images')
-        att.classifications = json_obj.get('classifications')
-        return att
-
-
 class Venue:
     """A Ticketmaster venue
     
@@ -769,138 +875,35 @@ class Event:
         return e
 
 
-class PageIterator:
-    """Iterates through API response pages"""
-    def __init__(self, api_client, **kwargs):
-        self.api_client = api_client  #: Parent API client
-        self.page = None  #: Current page
-        self.page = self.__page(**kwargs)
+class Attraction:
+    """Attraction object"""
 
-        self.start_page = self.page.number  #: Initial page number
-        self.current_page = self.start_page  #: Current page number
-        self.end_page = self.page.total_pages  #: Final page number
-
-    def __iter__(self):
-        return self
-
-    def limit(self, max_pages=10):
-        """Limit the number of page requests. Default: 5
-        
-        With a default page size of 20, ``limit(max_pages=5`` would 
-        return a maximum of 200 items (fewer, if there are fewer results).
-        
-        Use this to contain the number of API calls being made, as the 
-        API restricts users to a maximum of 5,000 per day. Very 
-        broad searches can return a large number of pages.
-        
-        To contrast, ``all()`` will automatically request every 
-        page available.
-        
-        :param max_pages: Maximum number of pages to request. 
-            Default: *10*. Set to *None* (or use ``all()``) to return 
-            all pages.
-        :return: Flat list of results from pages
+    def __init__(self, attraction_id=None, attraction_name=None, url=None,
+                 classifications=None, images=None, test=None):
         """
-        if max_pages is None:
-            return self.all()
 
-        all_items = []
-        for i in range(0, max_pages):
-            all_items += self.next()
-        return all_items
-
-    def all(self):
-        """Returns a flat list of all results. Queries all possible pages.
-        
-        Use ``limit()`` to restrict the number of calls being made.
-        
-        :return: Flat list of results
+        :param attraction_id: Attraction ID
+        :param attraction_name: Name of the attraction (ex: '*U2*'
+        :param url: Ticketmaster URL
+        :param classifications: Genres, subgenres, etc
+        :param images: Images
+        :param test: Test (bool)
         """
-        return [i for item_list in self for i in item_list]
+        self.id = attraction_id
+        self.name = attraction_name
+        self.url = url
+        self.classifications = classifications
+        self.images = images
+        self.test = test
 
     @staticmethod
-    def __page(**kwargs):
-        """Instantiate and return a Page(list)"""
-        page = kwargs['page']
-
-        links = kwargs['_links']
-
-        if 'next' not in links:
-            links_next = None
-        else:
-            links_next = links['next']['href']
-
-        return Page(
-            page['number'],
-            page['size'],
-            page['totalElements'],
-            page['totalPages'],
-            links['self']['href'],
-            links_next,
-            kwargs.get('_embedded', {})
-        )
-
-    def next(self):
-        # Return initial Page result if we haven't yet
-        if self.page.number == self.current_page:
-            self.current_page += 1
-            return [i for i in self.page]
-
-        # StopIteration if we know we've run out of pages.
-        # Check for current>end as empty results still return
-        # a page and increment the counter.
-        if self.current_page >= self.end_page:
-            raise StopIteration
-
-        # Otherwise, +1 our count and pull the next page
-        self.current_page += 1
-        r = requests.get(self.page.link_next,
-                         params=self.api_client.api_key).json()
-
-        self.page = self.__page(**r)
-
-        # If 'next' link goes missing, there were fewer pages than
-        # expected for some reason, so bump current_page to end_page to
-        # throw StopIteration next time next() is called
-        if self.page.link_next is None:
-            self.current_page = self.end_page
-
-        return [i for i in self.page]
-
-    __next__ = next
-
-
-class Page(list):
-    """API response page"""
-    def __init__(self, number, size, total_elements, total_pages,
-                 link_self, link_next, embedded):
-        super().__init__([])
-        self.number = number  #: Page number
-        self.size = size  #: Page size
-        self.total_elements = total_elements  #: Total elements (all pages)
-        self.total_pages = total_pages  #: Total pages
-
-        self._link_self = link_self  #: Link to this page
-        self._link_next = link_next  #: Link to the next page
-
-        # Parse embedded objects and add them to ourself
-        items = []
-        if 'events' in embedded:
-            items = [Event.from_json(e) for e in embedded['events']]
-        elif 'venues' in embedded:
-            items = [Venue.from_json(v) for v in embedded['venues']]
-        elif 'attractions' in embedded:
-            items = [Attraction.from_json(a) for a in embedded['attractions']]
-
-        for i in items:
-            self.append(i)
-
-    @property
-    def link_next(self):
-        """Link to the next page"""
-        return "{}{}".format(ApiClient.base_url, self._link_next)
-
-    @property
-    def link_self(self):
-        """Link to this page"""
-        return "{}{}".format(ApiClient.base_url, self._link_self)
+    def from_json(json_obj):
+        """Convert JSON object to ``Attraction`` object"""
+        att = Attraction()
+        att.id = json_obj.get('id')
+        att.name = json_obj.get('name')
+        att.url = json_obj.get('url')
+        att.test = json_obj.get('test')
+        att.images = json_obj.get('images')
+        att.classifications = json_obj.get('classifications')
+        return att
