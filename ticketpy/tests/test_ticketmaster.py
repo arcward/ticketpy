9a3941a1049aa76ee067f080e0012c2250c35de1
Edward Wells <git@edward.sh>
2017-04-13 01:37:39 -0400

    Revert "Added/updated unit tests"
    This reverts commit d1bf9ee0fc9422ad0c6583b2bf40f824706a1dd9.

---
+++ ticketpy/tests/test_ticketmaster.py
@@ -0,0 +1,62 @@
+from unittest import TestCase
+from configparser import ConfigParser
+import os
+from ticketpy import ticketpy
+from ticketpy.ticketpy import ApiException
+
+
+class TestTicketpy(TestCase):
+    def setUp(self):
+        config = ConfigParser()
+        config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
+        api_key = config.get('ticketmaster', 'api_key')
+
+        self.tm = ticketpy.ApiClient(api_key)
+        self.venues = {
+            'smithes': 'KovZpZAJledA',
+            'tabernacle': 'KovZpaFEZe'
+        }
+
+    def test_get_event_id(self):
+        e = self.tm.events.get('vvG1zZfbJQpVWp')
+        print(e)
+
+    def test_get_venue(self):
+        v = self.tm.venues.get('KovZpZAJledA')
+        print(v)
+
+    def test_iterator(self):
+        event_list = self.tm.events.find(venue_id=self.venues['tabernacle'],
+                                         size='7').all()
+        for e in event_list:
+            print(e.event_id)
+        
+    def test_search_events(self):
+        event_list = self.tm.events.find(venue_id=self.venues['tabernacle'],
+                                         size=1, include_tba=True).all()
+        for e in event_list:
+            print(str(e))
+
+    def test_events_get(self):
+        elist = self.tm.events.find(classification_name='Hip-Hop',
+                                    venue_id='KovZpZAJledA', size=7)
+        for e in elist:
+            print(e)
+        
+    def test_search_events_by_location(self):
+        event_list = self.tm.events.by_location(
+            latitude='33.7838737',
+            longitude='-84.366088',
+            radius=1,
+            unit='miles'
+        ).all()
+        for e in event_list:
+            print(e)
+
+    def test_venue_search(self):
+        venue_list = self.tm.venues.find(keyword="The Tabernacle",
+                                         state_code="GA",
+                                         size=1).all()
+        self.assertEqual(1, len(venue_list))
+        tabernacle = venue_list[0]
+        self.assertEqual("The Tabernacle", tabernacle.name)
